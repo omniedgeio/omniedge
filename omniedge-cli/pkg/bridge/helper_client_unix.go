e723f5019c3b30321501da683e907c323244ddf3
yongqianme <93888+yongqianme@users.noreply.github.com>
2026-01-08 11:33:50 +0800

    chore: remove subtree folder to prepare for root-level merge

--- omniedge-cli/pkg/bridge/helper_client_unix.go
+++
@@ -1,5 +0,0 @@
-//go:build !windows
-
-package bridge
-
-const HelperSocketPath = "/var/run/omniedge-helper.sock"
--- omniedge-cli/pkg/bridge/helper_client_windows.go
+++
@@ -1,6 +0,0 @@
-//go:build windows
-
-package bridge
-
-// Windows uses named pipes for IPC
-const HelperSocketPath = `\\.\pipe\omniedge-helper`
