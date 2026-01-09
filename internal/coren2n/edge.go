a34f8dbe2c00b91a6e6155ec197bbaa76f35ff85
yongqianme <93888+yongqianme@users.noreply.github.com>
2026-01-09 13:13:06 +0800

    fix: use git clone -b to clone specific n2n branch directly

--- internal/coren2n/edge.go
+++ internal/coren2n/edge.go
@@ -3,7 +3,7 @@
 
 package coren2n
 
-//go:generate sh -c "rm -rf n2n; git clone https://github.com/omniedgeio/n2n.git; cp make_n2n n2n/; cd n2n; chmod +x make_n2n; git checkout 2.6-stable-omni; if [ \"$(uname)\" = 'Darwin' ] && [ -f ../../patches/tuntap_osx_utun.c ]; then cp ../../patches/tuntap_osx_utun.c tuntap_osx.c; fi; if [ -f ../../patches/n2n_multicast.patch ]; then patch -p0 < ../../patches/n2n_multicast.patch; fi; ./make_n2n"
+//go:generate sh -c "rm -rf n2n; git clone -b 2.6-stable-omni https://github.com/omniedgeio/n2n.git; cp make_n2n n2n/; cd n2n; chmod +x make_n2n; if [ \"$(uname)\" = 'Darwin' ] && [ -f ../../patches/tuntap_osx_utun.c ]; then cp ../../patches/tuntap_osx_utun.c tuntap_osx.c; fi; if [ -f ../../patches/n2n_multicast.patch ]; then patch -p0 < ../../patches/n2n_multicast.patch; fi; ./make_n2n"
 
 /*
 #cgo CFLAGS: -g3 -Wall
