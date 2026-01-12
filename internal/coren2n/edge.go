e9ef651026a8f944dc66701e28ef1fd3b50ced26
yongqianme <93888+yongqianme@users.noreply.github.com>
2026-01-12 16:51:59 +0800

    fix: use absolute CGo include paths for coren2n and cleanup desktop release summary

--- internal/coren2n/edge.go
+++ internal/coren2n/edge.go
@@ -6,7 +6,7 @@ package coren2n
 //go:generate sh -c "rm -rf n2n; git clone -b 2.6-stable-omni https://github.com/omniedgeio/n2n.git; cp make_n2n n2n/; cd n2n; chmod +x make_n2n; ./make_n2n"
 
 /*
-#cgo CFLAGS: -g3 -Wall -In2n
+#cgo CFLAGS: -g3 -Wall -I${SRCDIR}/n2n
 #cgo LDFLAGS: -ln2n -L${SRCDIR}/n2n
 #include "edge.h"
 */
